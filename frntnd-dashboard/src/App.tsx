963754bfba84c0fa8512583432a37fc9f066c9c1
Alex Córdova <alexcordova111@gmail.com>
2024-06-25 00:25:36 -0500

    feat: Update App.css to adjust header styling
    The commit modifies the App.css file to adjust the styling of the header in the application. This change includes updating the background color, adding padding-top, updating the justify-content property, and setting the font-weight property. These changes improve the visual appearance and layout of the header section.

--- frntnd-dashboard/src/App.tsx
+++ frntnd-dashboard/src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SelectComponent from './components/option';
 
 function App() {
   return (
@@ -11,6 +12,41 @@ function App() {
         <div className="App">
           <div className="alert alert-primary" role="alert">
             <span>DPA por Parroquia - Cantón - Parroquia</span>
+          </div>
+        </div>
+        <div className="container">
+          <SelectComponent />
+          <select className="form-select" aria-label="Default select example">
+            <option selected>Canton</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </select>
+          <select className="form-select" aria-label="Default select example">
+            <option selected>Parroquia</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </select>
+        </div>
+        <div className='container-results'>
+          <div className='results-code'>
+            <span className="badge text-bg-danger">Codigo de Provincia</span>
+            <div className="alert alert-danger" role="alert">
+              2345
+            </div>
+          </div>
+          <div className='results-code'>
+            <span className="badge text-bg-danger">Codigo de Cantón</span>
+            <div className="alert alert-danger" role="alert">
+              2345
+            </div>
+          </div>
+          <div className='results-code'>
+            <span className="badge text-bg-danger">Codigo de Parroquia</span>
+            <div className="alert alert-danger" role="alert">
+              23546
+            </div>
           </div>
         </div>
       </header>
