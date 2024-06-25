17a02fc9ecde4f52dc83b0ddd541121f844020d5
Alex Córdova <alexcordova111@gmail.com>
2024-06-24 23:48:17 -0500

    refactor: Update App.css to adjust header styling
    The commit modifies the App.css file to adjust the styling of the header in the application. The background color is changed to #282c34, padding-top is added with a value of 10px, and the justify-content property is updated to flex-start. Additionally, the font-weight property is set to bold for the header text. These changes improve the visual appearance and layout of the header section.

--- frntnd-dashboard/src/App.tsx
+++ frntnd-dashboard/src/App.tsx
@@ -2,22 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App">
+          <div className="alert alert-primary" role="alert">
+            <span>DPA por Parroquia - Cantón - Parroquia</span>
+          </div>
+        </div>
       </header>
     </div>
   );
